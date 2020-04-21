a52d4c4d4e78f6b58686a89183feeab019ed1c32
Joseph Kim <josephkim570@gmail.com>
2020-04-21 10:01:27 -0400

    Added passport and googleStrategy for one option of authentication - as part of a gitignore file, included a keys.js per ClientID/Secret, changed the localhost port to 5000 as default

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express'),
     path = require('path'),
     cookieParser = require('cookie-parser'),
-    logger = require('morgan');
+    logger = require('morgan'),
+    passport = require('passport'),
+    GoogleStrategy = require('passport-google-oauth20').Strategy,
+    keys = require('./config/keys');
 
 const indexRouter = require('./routes/index'),
     usersRouter = require('./routes/users');
 
 const app = express();
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access token', accessToken);
+            console.log('refresh token', refreshToken);
+            console.log('profile:', profile);
+        }
+    )
+);
 
 app.use(logger('dev'));
 app.use(express.json());
