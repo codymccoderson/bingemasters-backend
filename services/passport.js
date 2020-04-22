6edcb70f91b0d4c4080a76a1434463f2545119f2
Joseph Kim <josephkim570@gmail.com>
2020-04-22 06:02:47 -0400

    Added SerializeUser and DeserializeUser methods of Passport to store sessions based on the unique MongoDB assigned ids'per the google profile ids' and added a query method based on the mongoose model to search through the database for users ids that exist for login purposes (cookie mgmt)

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const passport = require('passport'),
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -20,8 +31,11 @@ passport.use(
             User.findOne({ googleId: profile.id })
                 .then((existingUser) => {
                     if (existingUser) {
+                        done(null, existingUser);
                     } else {
-                       new User({ googleId: profile.id }).save(); 
+                       new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
                     
 
                     }
