9340e936ee3f57f4a5fde7801ef520ca5dd008bc
Joseph Kim <josephkim570@gmail.com>
2020-04-23 13:38:07 -0400

    Added react-redux, redux, redux-thunk, axios - Added another proxy to handle all /api pathing which will be responsible for tracking user permissions - added a redux store with authReducer - Setup an actions directory for the action types and ajax calls

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider  } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
+
+// stand in data
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
